b8e787e11890dd14a103b6481f9489d93a7a27ae
Stoo <stoo.goff@gmail.com>
2021-03-30 23:43:47 +0100

    Layout

--- pages/characters/new/attributes.jsx
+++ pages/characters/new/attributes.jsx
@@ -17,10 +17,30 @@ const Attributes = () => {
 	return <Layout title="3: Attributes">
 		<p>The character has four Attributes - <strong>Control</strong>, <strong>Focus</strong>, <strong>Passion</strong> and <strong>Strength</strong> which are rated between { ATTRIBUTE_MIN } and { ATTRIBUTE_MAX}.</p>
 		<h3 className={ remaining < 0 ? 'text-red-500' : 'text-green-500' }>Points remaining: { remaining }</h3>
-		<Attribute onChange={ setAttribute('Control') } value={ character.attributes.Control } title="Control">Control is about physical dexterity, coordination, accuracy and control.</Attribute>
-		<Attribute onChange={ setAttribute('Strength') } value={ character.attributes.Strength } title="Strength">Strength is all about physical strength, power, force and endurance.</Attribute>
-		<Attribute onChange={ setAttribute('Focus') } value={ character.attributes.Focus } title="Focus">Focus is all about thinking, mental reasoning, emotional restraint, and concentrating.</Attribute>
-		<Attribute onChange={ setAttribute('Passion') } value={ character.attributes.Passion } title="Passion">Passion is all about determination, mental force, emotional strength and imagination.</Attribute>
+		<Attribute
+			onChange={ setAttribute('Control') }
+			value={ character.attributes.Control }
+			title="Control">
+			Control is about physical dexterity, coordination, accuracy and control.
+		</Attribute>
+		<Attribute
+			onChange={ setAttribute('Strength') }
+			value={ character.attributes.Strength }
+			title="Strength">
+			Strength is all about physical strength, power, force and endurance.
+		</Attribute>
+		<Attribute
+			onChange={ setAttribute('Focus') }
+			value={ character.attributes.Focus }
+			title="Focus">
+			Focus is all about thinking, mental reasoning, emotional restraint, and concentrating.
+		</Attribute>
+		<Attribute
+			onChange={ setAttribute('Passion') }
+			value={ character.attributes.Passion }
+			title="Passion">
+			Passion is all about determination, mental force, emotional strength and imagination.
+		</Attribute>
 		<Button href='/characters/new/abilities' disabled={ remaining !== 0 || notAllValid }>Next</Button>
 	</Layout>
 }
