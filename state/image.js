d3d1ec80dd8dd236587180539480731a8d97ab64
Stoo <stoo.goff@gmail.com>
2021-05-15 11:19:49 +0100

    Cache images

--- state/image.js
+++ state/image.js
@@ -1,20 +1,36 @@
 
+import Vue from 'vue'
 import { storage } from '~/plugins/firebase'
 import { STORAGE_CHARACTERS } from '~/utils/config'
 
+const state = Vue.observable({
+	images: [],
+})
+
+
 export default {
 	async byPath(path) {
+		const existingImage = state.images.find(img => img.path == path)
+
+		if(existingImage) {
+			return existingImage
+		}
+
 		const root = storage().ref(STORAGE_CHARACTERS)
 		const image = await root.child(path).getDownloadURL()
 		const metadata = await root.child(path).getMetadata()
 
-		return {
+		const newImage = {
 			path,
 			url: image,
 			size: metadata.size,
 			type: metadata.contentType,
 			lastModified: Date.parse(metadata.updated),
 		}
+
+		state.images = [ ...state.images, newImage ]
+
+		return newImage
 	},
 
 	async save({ id, file }) {
@@ -31,5 +47,6 @@ export default {
 
 	async delete(imagePath) {
 		await storage().ref(STORAGE_CHARACTERS).child(imagePath).delete()
+		state.images = state.images.filter(image => image.path !== imagePath)
 	},
 }
