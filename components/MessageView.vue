2c45dd09f9fd8d80a8d4bb710cad7d823e57f4eb
Stoo <stoo.goff@gmail.com>
2021-11-14 17:35:21 +0000

    Increased z-index of message

--- components/MessageView.vue
+++ components/MessageView.vue
@@ -1,6 +1,6 @@
 <template>
 	<slide-up>
-		<div class="fixed bottom-2 left-4 right-4 flex justify-center">
+		<div class="fixed bottom-2 left-4 right-4 flex justify-center z-50">
 			<div class="p-2 shadow-lg w-full max-w-xl alert success">
 				<slot />
 			</div>
